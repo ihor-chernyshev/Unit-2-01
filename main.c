f6c1b99842bcad2b279aa80d3a68b8c6822f9edb
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2025-02-11 17:55:39 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+//Created by Ihor Chernyshev
+//Created on Feb 11 2025
+//This program turns the LED on and off
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
